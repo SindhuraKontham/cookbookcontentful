a4dffd9a961718304c780e8d356fae386d4c35b4
Simran Suky Kaur <simransukhi26@gmail.com>
2023-01-10 16:47:39 +0100

    salad-feature

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import Header from "./components/navbar/Header";
-import {Routes,Route} from 'react-router-dom';
-import Soup from './components/soup/Soup';
-import Salad from './components/salad/Salad';
-import Starter from './components/starter/Starter';
-import Main from './components/main/Main';
+import { Routes, Route } from "react-router-dom";
+import Soup from "./components/soup/Soup";
+import Salad from "./components/salad/Salad";
+import Starter from "./components/starter/Starter";
+import Main from "./components/main/Main";
 import Home from "./components/home/Home";
 import Footer from "./components/footer/Footer";
-
+import SaladPage from "./components/salad/SaladPage";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div>
-      
- <Header />
- <Routes>
-   <Route path="/"  element={<Home />} />
-    <Route path='/soup' element={<Soup />} />
-    <Route path='/salad' element={<Salad />} />
-    <Route path='/starter' element={<Starter />} />
-    <Route path='/main' element={<Main />} />
-    </Routes>
-  <Footer />
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/soup" element={<Soup />} />
+        <Route path="/salad" element={<Salad />} />
+        <Route path="/starter" element={<Starter />} />
+        <Route path="/main" element={<Main />} />
+        <Route path="/salad/:id" element={<SaladPage />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
